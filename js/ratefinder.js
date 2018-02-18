230d5f31876437c07a7dc86cebe068270a2cd4b3
Beata Karolczak <karolczak.beata@gmail.com>
2018-02-18 22:34:37 +0100

    Add Part 2 from Using Promises

--- js/ratefinder.js
+++ js/ratefinder.js
@@ -1,7 +1,6 @@
-let url = "rates.json";
-    
-fetch(url)
-    .then(response => response.json())
+import * as service from './rate-service-mock';
+
+service.findAll()
     .then(rates => {
       let html = '';
       rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
